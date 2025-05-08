faec7a31ba3879fb2a09461ae8400189eb2f72c7
vCardEmpresarial <141480022+vCardEmpresarial@users.noreply.github.com>
2025-05-08 02:53:26 -0600

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ const { OpenAI } = require('openai');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'https://glitchwitch.onrender.com'
+}));
+
 app.use(bodyParser.json());
 
 const openai = new OpenAI({
